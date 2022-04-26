e26b09055a587cab6f171f745dcd55c55c7fef63
coremore35 <48387110+coremore35@users.noreply.github.com>
2022-04-26 17:33:39 -0400

    initial changes and updates to webpack/pkgjson

--- client/containers/MainContainer.js
+++ client/containers/MainContainer.js
@@ -4,55 +4,52 @@
  * @module  MainContainer
  * @author
  * @date
- * @description stateful component that renders 
+ * @description stateful component that renders
  *
  * ************************************
  */
 
- import React, {Component} from "react";
- import SearchBar from '/client/components/songSearchBar.js'
- import { connect } from 'react-redux';
- import * as actions from '../actions/actions.js';
- import AllReviews from '/client/containers/AllReviewsContainer.js'
-import SubmitReviewContainer from "./submitReviewContainer.js";
+import React, { Component } from 'react';
+import SearchBar from '/client/components/songSearchBar.js';
+import { connect } from 'react-redux';
+import * as actions from '../actions/actions.js';
+import AllReviews from '/client/containers/AllReviewsContainer.js';
+import SubmitReviewContainer from './submitReviewContainer.js';
 //import usersDashboard from '../client/components/usersDashboard.js';
 import '../scss/MainContainer.scss';
- 
-const mapStateToProps = state => ({
-    reviews : state.review.queriedReviews
+
+const mapStateToProps = (state) => ({
+  reviews: state.review.queriedReviews,
 });
 
-
-const mapDispatchToProps = dispatch => ({
-    queryReviews : (params) => dispatch(actions.queryReviewsActionDispatch(params))
-   });
+const mapDispatchToProps = (dispatch) => ({
+  queryReviews: (params) =>
+    dispatch(actions.queryReviewsActionDispatch(params)),
+});
 
 class MainContainer extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return(
-          <div>
-             <nav>
-             <SearchBar
-                        queryReviews = {this.props.queryReviews}
-                        state = {this.props.reviews}
-            ></SearchBar>
-            </nav>
-            <div id="main">
-                {/* <usersDashboard /> */}
-                <div className = "showReviews">
-             <AllReviews></AllReviews>
-             </div>
-             <SubmitReviewContainer />
-            </div>
-
-
-
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <nav>
+          <SearchBar
+            queryReviews={this.props.queryReviews}
+            state={this.props.reviews}
+          ></SearchBar>
+        </nav>
+        <div id='main'>
+          {/* <usersDashboard /> */}
+          <div className='showReviews'>
+            <AllReviews></AllReviews>
           </div>
-        );
-      }
+          <SubmitReviewContainer />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,57 @@
-const path = require ('path');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: './client/index.js',
-    output: {
-        filename: './main.js',
-        path: path.resolve(__dirname, 'public')
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    filename: './main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+      publicPath: '/',
     },
-    devServer: {
-        static: {
-          directory: path.resolve(__dirname, 'public'),
-          publicPath: '/'
+    port: 8080,
+    compress: true,
+    proxy: {
+      //changes domain request comes from
+      '/api': 'http://localhost:3000',
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      title: 'development',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: [
+            '@babel/plugin-transform-runtime',
+            '@babel/transform-async-to-generator',
+          ],
         },
-        port: 8080,
-        compress: true,
-        proxy: { //changes domain request comes from
-            
-            '/': 'http://localhost:3000'
-
-        }
-     },
-    plugins: [new HtmlWebpackPlugin({ template: './client/index.html', title: 'development'})],
-    module: {
-        rules: [
-                {
-                    test: /\.jsx?/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
-                         plugins: ['@babel/plugin-transform-runtime', '@babel/transform-async-to-generator'],
-                    }
-                },
-                {
-                    test: /.(css|scss)$/,
-                    exclude: /node_modules/,
-                    use: ['style-loader', 'css-loader', 'sass-loader'],
-                },
-        ]
-    },
-
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
 };
