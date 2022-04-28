3a45587aa07c2331c0699777c02fc08c32d863ce
Anu Sharma <anulepau@gmail.com>
2022-04-28 08:16:49 -0700

    updated with routes

--- client/components/Songs.jsx
+++ client/components/Songs.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
-import SeeReviews from './SeeReviews';
+import { Link } from 'react-router-dom';
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Songs = (props) => {
-    // add button 'Add Review', invoke a handleclick function from MainContainer that returns reviews
+    let artists_str = '';
+    for (let i = 0; i < props.artists.length; i++) {
+      if (i === props.artists.length - 1) {
+        artists_str +=  props.artists[i];
+      }
+      else artists_str += props.artists[i] + ', ';
+    }
+
     return(
-        <div>
-            {props.title}
-            <SeeReviews handleSeeReviews={props.handleSeeReviews} id={props.id} /> 
+        <div id="eachTrack">
+          <Card sx={{ maxWidth: 200 }}>
+            <CardMedia
+              component="img"
+              height="200"
+              image="https://i.scdn.co/image/ab67616d0000b27314bd90935ed5a2ac6c662373" // {props.albumCover}
+            />
+
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                {props.track}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                {artists_str}
+              </Typography>
+              <Typography gutterBottom variant="caption" component="div">
+                {props.albumName}
+              </Typography>
+            </CardContent>
+
+            <CardActions>
+              <Link to='/song/reviews'><Button size="small" onClick={ () => props.handleSeeReviews(props.track, artists_str, props.id)}>REVIEWS</Button></Link>
+            </CardActions>   
+          </Card>    
         </div>
     )
 }
